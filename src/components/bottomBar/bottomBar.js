53ba414226b6c716510c990848f2bc73a1c93125
sergey-pimenov <pimenov.web@gmail.com>
2018-08-22 18:22:54 +0300

    Add install button for Android

--- src/components/bottomBar/bottomBar.js
+++ src/components/bottomBar/bottomBar.js
@@ -18,28 +18,14 @@ var bottomBar = {
       var installPromptEvent;
 
       window.addEventListener('beforeinstallprompt', (event) => {
-        // Prevent Chrome <= 67 from automatically showing the prompt
         event.preventDefault();
-        // Stash the event so it can be triggered later.
         installPromptEvent = event;
-        alert('wrk')
-        // Update the install UI to notify the user app can be installed
-        // document.querySelector('#install-button').disabled = false;
+        document.body.classList.add('installApproved');
       });
 
       installButtonNode.addEventListener('click', () => {
-        // Update the install UI to remove the install button
-        // document.querySelector('#install-button').disabled = true;
-        // Show the modal add to home screen dialog
         installPromptEvent.prompt();
-        // Wait for the user to respond to the prompt
         installPromptEvent.userChoice.then((choice) => {
-          if (choice.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-          // Clear the saved prompt since it can't be used again
           installPromptEvent = null;
         });
       });
