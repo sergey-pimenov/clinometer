5a6690c0c40ac23ec1c3f54370a053d87cc0f090
sergey-pimenov <pimenov.web@gmail.com>
2018-08-25 23:54:04 +0300

    Create x and y axis colors and add cube

--- src/components/indicator/indicator.js
+++ src/components/indicator/indicator.js
@@ -1,5 +1,10 @@
-var rotateDataNode = document.querySelector('.rotateData');
-var detailsRotateNode = document.querySelector('.detailsRotate');
+import setCSSVar from './../../utils/scripts/setCSSVar';
+
+var rotateDataYNode = document.querySelector('.rotateDataY');
+var rotateDataXNode = document.querySelector('.rotateDataX');
+var detailsRotateYNode = document.querySelector('.detailsRotateY');
+var detailsRotateXNode = document.querySelector('.detailsRotateX');
+var cubeNode = document.querySelector('.cube');
 
 var indicator = {
   degreeY : 0,
@@ -7,6 +12,11 @@ var indicator = {
   renderedDegreeYRounded : null,
   degreeYAccurate : 0,
   renderedDegreeYAccurate : null,
+  degreeX: 0,
+  degreeXRounded: 0,
+  renderedDegreeXRounded: null,
+  degreeXAccurate: 0,
+  renderedDegreeXAccurate: null,
 
   init() {
     indicator.setListeners();
@@ -23,21 +33,38 @@ var indicator = {
     indicator.degreeY = e.beta;
     indicator.degreeYRounded = Math.round(indicator.degreeY);
     indicator.degreeYAccurate = indicator.degreeY.toFixed(1);
+
+    indicator.degreeX = e.gamma;
+    indicator.degreeXRounded = Math.round(indicator.degreeX);
+    indicator.degreeXAccurate = indicator.degreeX.toFixed(1);
   },
 
   update() {
     requestAnimationFrame(indicator.update);
 
     if (indicator.renderedDegreeYRounded != indicator.degreeYRounded) {
-      rotateDataNode.innerHTML = indicator.degreeYRounded;
+      rotateDataYNode.innerHTML = indicator.degreeYRounded;
+      setCSSVar(cubeNode, 'y', -indicator.degreeYRounded + 'deg');
+    }
+
+    if (indicator.renderedDegreeXRounded != indicator.degreeXRounded) {
+      rotateDataXNode.innerHTML = indicator.degreeXRounded;
+      setCSSVar(cubeNode, 'x', indicator.degreeXRounded + 'deg');
     }
 
     if (indicator.renderedDegreeYAccurate != indicator.degreeYAccurate) {
-      detailsRotateNode.innerHTML = indicator.degreeYAccurate;
+      detailsRotateYNode.innerHTML = indicator.degreeYAccurate;
+    }
+
+    if (indicator.renderedDegreeXAccurate != indicator.degreeXAccurate) {
+      detailsRotateXNode.innerHTML = indicator.degreeXAccurate;
     }
 
     indicator.renderedDegreeYRounded = indicator.degreeYRounded;
     indicator.renderedDegreeYAccurate = indicator.degreeYAccurate;
+
+    indicator.renderedDegreeXRounded = indicator.degreeXRounded;
+    indicator.renderedDegreeXAccurate = indicator.degreeXAccurate;
   }
 }
 
