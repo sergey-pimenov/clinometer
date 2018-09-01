3a6360a4aff20551a4509a3b4f0f39beccbd0897
sergey-pimenov <pimenov.web@gmail.com>
2018-09-02 00:22:45 +0300

    Show install button by default when beforeinstallprompt fired

--- src/components/actionBar/actionBar.js
+++ src/components/actionBar/actionBar.js
@@ -17,10 +17,15 @@ var actionBar = {
     } else {
       var installPromptEvent;
 
+      if (localStorage.getItem('installable')) {
+        document.body.classList.add('installable');
+      }
+
       window.addEventListener('beforeinstallprompt', (event) => {
         event.preventDefault();
         installPromptEvent = event;
         document.body.classList.add('installApproved');
+        localStorage.setItem('installable', true);
       });
 
       installButtonNode.addEventListener('click', () => {
